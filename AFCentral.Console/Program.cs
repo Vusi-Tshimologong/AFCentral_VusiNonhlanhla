145efef3fb254f1aa4f16db017127eb3d656e10f
barbara-tshimologong <barbara@tshimologong.joburg>
2022-05-24 10:44:04 +0200

    Add project files.

---
+++ AFCentral.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AFCentral.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Please enter your name");
+
+            var firstName = System.Console.ReadLine();
+            
+            System.Console.WriteLine("Please enter your surname");
+            var surName = System.Console.ReadLine();
+
+
+            System.Console.WriteLine("Please enter your Printer Code");
+                            var printerCode = System.Console.ReadLine();
+
+            DataAccess.SqlServerAccess ssa = new(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AFCentral;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            var msg = ssa.ConnectToDB(firstName, surName, printerCode);
+
+            System.Console.WriteLine(msg);
+        }
+    }
+}
---
+++ DataAccess/SqlServerAccess.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DataAccess
+{
+    public class SqlServerAccess
+    {
+
+        public string ConnectionString { get; init; }
+
+        public SqlServerAccess(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+
+        public string ConnectToDB(string firstName, string surname, string printerCode)
+
+        {
+
+            string msg = "unsuccessful";
+
+            using (SqlConnection cn = new SqlConnection() )
+            {
+                cn.ConnectionString = ConnectionString;
+                cn.Open();
+
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = cn;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "spInsert_Facilitator";
+
+                    SqlParameter param = new SqlParameter();
+                    param.ParameterName = "@FirstName";
+                    param.Value = firstName;
+
+                    cmd.Parameters.Add(param);
+
+                    SqlParameter param2 = new SqlParameter();
+                    param2.ParameterName = "@Surname";
+                    param2.Value = surname;
+
+                    cmd.Parameters.Add(param2);
+
+                    SqlParameter param3 = new SqlParameter();
+                    param3.ParameterName = "@PrinterCode";
+                    param3.Value = printerCode;
+
+                    cmd.Parameters.Add(param3);
+
+                    SqlParameter param4 = new SqlParameter();
+                    param4.ParameterName = "@Msg";
+                    param4.Value = String.Empty;
+
+                    cmd.Parameters.Add(param4);
+
+
+                    cmd.ExecuteNonQuery();
+
+                    return "success";
+                }
+            }
+            return msg;
+        }
+    }
+}
